53ce00c09073dfb3c579a4078ea5dd0231921ab7
growwithsdtr <growwithsdtr@gmail.com>
2026-06-09 12:10:54 +0900

    v5.2 clean unused heavy assets

--- src/main.cpp
+++ src/main.cpp
@@ -17,7 +17,7 @@
 namespace {
 constexpr uint32_t kSerialBaud = 115200;
 constexpr uint32_t kSdSpiHz = 25000000;
-constexpr const char* kFirmwareVersion = "v5.1";
+constexpr const char* kFirmwareVersion = "v5.2";
 constexpr const char* kBadgeJsonPath = "/paperbadge/badge.json";
 constexpr const char* kCoachDeckPath = "/papercoach/decks/interview_cards.json";
 constexpr const char* kLegacyCoachDeckPath = "/papercoach/decks/sample_interview.json";
